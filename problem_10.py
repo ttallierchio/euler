a33a54353ebb3959dcb5050a41879c0678ae95fc
Tony Tallierchio <Tony.Tallierchio@gmail.com>
2016-09-06 20:03:06 -0400

    problem 10. sieve

---
+++ problem_10.py
@@ -0,0 +1,17 @@
+def primes_sieve(limit):
+    limitn = limit+1
+    not_prime = set()
+    primes = []
+
+    for i in range(2, limitn):
+        if i in not_prime:
+            continue
+
+        for f in range(i*2, limitn, i):
+            not_prime.add(f)
+
+        primes.append(i)
+
+    return primes
+a = primes_sieve(2000000)
+print sum(a)
